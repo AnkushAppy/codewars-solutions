da84263a1f9c5deae2597c50af3ea25c3c860ce6
Ankush Patel <sabretooth.rog@gmail.com>
2016-09-07 02:11:48 +0530

    Create dont_rely_on_luck.py

---
+++ dont_rely_on_luck.py
@@ -0,0 +1,17 @@
+"""
+https://www.codewars.com/kata/dont-rely-on-luck/python
+The test fixture I use for this kata is pre-populated.
+In Python by:
+
+randint(1,100)
+You can pass by relying on luck or skill but try not to rely on luck.
+
+"The power to define the situation is the ultimate power." - Jerry Rubin
+
+Good luck!
+"""
+
+"""
+Learned random class. its methods like getstate, setstate, seed.
+Also function overiding.
+"""
